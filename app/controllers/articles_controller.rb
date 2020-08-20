9e12a635dda1e247f6b05ee14d8f191bf6ec2d34
Ricky DeFazio <36946678+RickyDeFazio@users.noreply.github.com>
2020-08-20 13:17:16 -0400

    convert ERB files to HAML files

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,4 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: 'rsd', password: 'secret', except: %i[index show]
-
   def index
     @articles = Article.all
   end
